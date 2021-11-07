00a1b4850c8d0f09c78a134ed83bb8f06d36d0ec
mark nurenberg <marknure93@gmail.com>
2021-11-07 19:15:12 +0200

    first change af matala

--- hello.java
+++ hello.java
@@ -3,6 +3,7 @@ public static void main (String [] args) {
 		for (int i=1;i<=10; i++) {
 					System.out.println("hello world.."+i);
 					System.out.println("NEXT STEP IS APDATE THE JENKINS.."+i);
+					System.out.println("First Change of matal"+i);
 		}
 	}	
 }
