8a4b0a70555ac80330bb204ba6508bc6086bddeb
mark nurenberg <marknure93@gmail.com>
2021-11-07 19:05:24 +0200

    we make next step

--- hello.java
+++ hello.java
@@ -1,7 +1,8 @@
 public class hello {
 public static void main (String [] args) {
 		for (int i=1;i<=10; i++) {
-					System.out.println("hello world..."+i);
+					System.out.println("hello world.."+i);
+					System.out.println("NEXT STEP IS APDATE THE JENKINS.."+i);
 		}
 	}	
 }
