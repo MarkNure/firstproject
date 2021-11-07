5d3874283b005a30586a50a41ed05394b4461a21
mark nurenberg <marknure93@gmail.com>
2021-11-07 19:23:55 +0200

    next change of matala

--- hello.java
+++ hello.java
@@ -3,7 +3,8 @@ public static void main (String [] args) {
 		for (int i=1;i<=10; i++) {
 					System.out.println("hello world.."+i);
 					System.out.println("NEXT STEP IS APDATE THE JENKINS.."+i);
-					System.out.println("First Change of matal"+i);
+					System.out.println("First Change of matala"+i);
+                                                                        System.out.println("First Change of matala2"+i);
 		}
 	}	
 }
